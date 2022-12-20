8c0eb850bf455077b606628d6b2e2a0f18d729e3
Kartik <114608483+Kartik4075@users.noreply.github.com>
2022-12-20 13:57:00 -0600

    Add files via upload

---
+++ go.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var age int = 29 // variable declaration with initial value
+
+	fmt.Println("My age is", age)
+}
+
+func calculateBill(price int, no int) int {  
+    var totalPrice = price * no
+    return totalPrice
+}
+
+
+package main 
+
+import "fmt"
+
+func main() {  
+    fmt.Println("Simple interest calculation")
+}
